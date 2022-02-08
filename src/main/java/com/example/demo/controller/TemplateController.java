70700d2aead57788388d31de75d966e0191f8c85
Aleksandr Gubenko <rusf48@live.ru>
2022-02-08 15:58:05 +0300

    test

--- src/main/java/com/example/demo/controller/TemplateController.java
+++ src/main/java/com/example/demo/controller/TemplateController.java
@@ -1,20 +1,26 @@
 package com.example.demo.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping()
 public class TemplateController {
 
-//    @GetMapping("/login")
-//    public String getLoginView() {
-//        return "login";
-//    }
-//
-//    @GetMapping("/courses")
-//    public String getCourses() {
-//        return "courses";
-//    }
+    @GetMapping("/user/info")
+    public ResponseEntity<String> getLoginView() {
+        return new ResponseEntity<>("{\n" +
+                "    \"username\": \"user\",\n" +
+                "    \"password\": \"pass\"\n" +
+                "}", HttpStatus.OK);
+    }
+
+    @GetMapping("/courses")
+    public String getCourses() {
+        return "courses";
+    }
 }
